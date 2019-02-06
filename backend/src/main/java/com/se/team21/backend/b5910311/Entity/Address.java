0ca91b645e4e802406f798b0965b142bae6e0954
paethaweechai <pae_tawee@hotmail.co.th>
2019-02-06 23:12:30 +0700

    close #99 -แก้ไข address entity

--- backend/src/main/java/com/se/team21/backend/b5910311/Entity/Address.java
+++ backend/src/main/java/com/se/team21/backend/b5910311/Entity/Address.java
@@ -14,21 +14,29 @@ public class Address {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_seq")
     @Column(name="Address_ID",unique = true, nullable = true)
     private @NonNull Long aid;
+    private @NonNull String code;
     private @NonNull String addressname;
 
     public Address() {
     }
 
-    public Address(Long aid,String addressname) {
+    public Address(Long aid,String addressname,String code) {
         this.aid = aid;
+        this.code = code;
         this.addressname = addressname;
     }
-    public String getAddress() {
+    public String getAddressname() {
         return addressname;
     }
-    public void setAddress(String addressname) {
+    public void setAddressname(String addressname) {
         this.addressname = addressname;
     }  
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    } 
     public void setAid(Long aid) {
         this.aid = aid;
     }
