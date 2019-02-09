af618f72fefde9517d07c058045a8877448eebba
pattaraphong <illsnick037@gmail.com>
2019-02-09 14:16:42 +0700

    add Entity RegisterMedia close #85

---
+++ backend/src/main/java/com/se/team21/backend/B5901890/Entity/KindSportMedia.java
@@ -0,0 +1,40 @@
+package com.se.team21.backend.B5901890.Entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name="KindSportMedia")
+@ToString
+@Getter @Setter
+@EqualsAndHashCode
+public class KindSportMedia {
+    @Id
+    @SequenceGenerator(name="KindSportMedia_seq",sequenceName="KindSportMedia_seq")
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="KindSportMedia_seq")
+    @Column(name="KindSportMedia_Id",unique = true, nullable = false)
+
+    private @NonNull Long id_Sport;
+    private @NonNull String kind_Sport;
+
+    public Long getId_Sport() {
+        return id_Sport;
+    }
+
+    public void setId_Sport(Long id_Sport) {
+        this.id_Sport = id_Sport;
+    }
+
+    public String getKind_Sport() {
+        return kind_Sport;
+    }
+
+    public void setKind_Sport(String kind_Sport) {
+        this.kind_Sport = kind_Sport;
+    }
+
+    public KindSportMedia(){}
+
+}
