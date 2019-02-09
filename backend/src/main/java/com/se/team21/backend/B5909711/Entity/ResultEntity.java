8aa3e9ca6817c2534bd5b1917673cf2a8dce813a
narawichsaphimarn <NarawichSaphimarn@gmail.com>
2019-02-09 17:14:29 +0700

    ทำ การเพิ่ม Validation Annotation ให้กับ ระบบบันทึกผลการแข่งขัน - close #127

--- backend/src/main/java/com/se/team21/backend/B5909711/Entity/ResultEntity.java
+++ backend/src/main/java/com/se/team21/backend/B5909711/Entity/ResultEntity.java
@@ -3,6 +3,9 @@ import lombok.*;
 import com.se.team21.backend.b5910311.entity.SportsEvent;
 import com.se.team21.backend.b5910311.entity.SportsType;
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Entity
@@ -18,34 +21,48 @@ public class ResultEntity {
     @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="result_seq")
     @Column(name="resultID",unique = true, nullable = false)
     private @NonNull Long resultID;
+
+    @NotNull
+    @Column(unique = true)
     private @NonNull String resultName;
+
+    @NotNull
     @Temporal(TemporalType.DATE)
     private @NonNull Date resultDate;
+
+    @NotNull
+    @Pattern(regexp = "[ก-๙]+|[A-Z|a-z|' '|1-9]+")
+    @Size(min=2, max=20)
     private @NonNull String resultAddress;
 
     //Many To One with SportsEvent
     @ManyToOne /*(fetch = FetchType.EAGER, targetEntity = AgencyTypeEntity.class)*/
     @JoinColumn(name = "seid")
+    @NotNull(message="SportId must not be null to be valid")
     private SportsEvent sportsEvent;
 
     //Many To One with Province
     @ManyToOne /*(fetch = FetchType.EAGER, targetEntity = ProvinceEntity.class)*/
     @JoinColumn(name = "provinceID")
+    @NotNull(message="SportId must not be null to be valid")
     private ProvinceEntity provinceEntity;
 
     //Many To One with SportsType
     @ManyToOne /*(fetch = FetchType.EAGER, targetEntity = ProvinceEntity.class)*/
     @JoinColumn(name = "stid")
+    @NotNull(message="SportId must not be null to be valid")
     private SportsType sportsType;
 
     //Many To One with Rating
     @OneToOne /*(fetch = FetchType.EAGER, targetEntity = AgencyTypeEntity.class)*/
     @JoinColumn(name = "rid")
+    @NotNull(message="SportId must not be null to be valid")
     private RatingEntity ratingEntity;
 
     //Many To One with PersonType
     @OneToOne /*(fetch = FetchType.EAGER, targetEntity = AgencyTypeEntity.class)*/
     @JoinColumn(name = "personTypeID")
+    @NotNull(message="SportId must not be null to be valid")
     private PersonTypeEntity personTypeEntity;
 
     public Long getResultID() {
